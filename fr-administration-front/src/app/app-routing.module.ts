abe6ceea16e3723b08d3ededb1d5889e4aeddb46
vld <valentin.lorand@kereval.com>
2022-01-27 11:30:59 +0100

    Setup guard to protect urls

--- fr-administration-front/src/app/app-routing.module.ts
+++ fr-administration-front/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UsersListComponent } from './users-list/users-list.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path: '',component: LoginComponent},
-  {path: 'users',component: UsersListComponent},
+  {path: 'users',component: UsersListComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
