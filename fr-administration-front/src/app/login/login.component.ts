9954483f197c5c7d897925487bd9be59bfcd1fa9
vld <valentin.lorand@kereval.com>
2022-01-27 11:25:17 +0100

    Improve login page

--- fr-administration-front/src/app/login/login.component.ts
+++ fr-administration-front/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiHelperService } from '../services/api-helper.service';
+import { TokenStorageService } from '../services/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: ApiHelperService,private tokenStorageService: TokenStorageService,private route: Router) { }
 
   login(): void {
-    console.log('click on login !')
-}
+    const username: string = (document.getElementById('username') as HTMLInputElement).value;
+    const password: string = (document.getElementById('password') as HTMLInputElement).value;
 
-  ngOnInit(): void {
+    const result = this.api.post({endpoint: '/auth/login', data: { username, password },}).then(response => {
+        this.tokenStorageService.save(response.access_token);
+        if(this.tokenStorageService.isLogged()) {
+          this.route.navigateByUrl("/users")
+        } else {
+          (document.getElementById('message') as HTMLInputElement).value = "Le mot de passe est incorrect";
+        }
+    });
   }
+
+  ngOnInit(): void {}
 
 }
