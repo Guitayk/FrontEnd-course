9f26ca4ccf334efe9a743892edec0efe5674ac31
vld <valentin.lorand@kereval.com>
2022-01-29 22:24:31 +0100

    Improve users-list component

--- fr-administration-front/src/app/users-list/users-list.component.ts
+++ fr-administration-front/src/app/users-list/users-list.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { User } from '../dto/User';
 import { UsersService } from '../services/users.service';
@@ -11,12 +13,55 @@ import { UsersService } from '../services/users.service';
 })
 export class UsersListComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'lastname', 'firstname', 'age'];
+  displayedColumns: string[] = ['id', 'lastname', 'firstname', 'age','actions'];
   dataSource: User[] = [];
 
-  constructor(private http: HttpClient, private userService: UsersService) {}
+  constructor(public dialog: MatDialog,private userService: UsersService) {}
+
+
+  deleteAssociation(id:number) {
+    this.userService.deleteUser(id);
+    this.dataSource = this.dataSource.filter(x => x.id != id);
+  }
+
+  openDialog(association:User): void {
+    const dialogRef = this.dialog.open(SeeUserDialog, {
+      width: '400px',
+      data: association,
+    });
+
+    dialogRef.afterClosed().subscribe((result:User | undefined) => {
+      if(result !== undefined) {
+        const index = this.dataSource.findIndex(x => x.id == result.id);
+        this.dataSource[index].firstname = result.firstname;
+        this.dataSource[index].lastname = result.lastname;
+        this.dataSource[index].age = result.age;
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.userService.searchUsers().subscribe(result => this.dataSource =result);
+  }
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'see-user.dialog.html',
+  styleUrls: ['./users-list.component.scss']
+})
+export class SeeUserDialog {
+
+  firstnameControl = new FormControl(this.data.firstname);
+  lastnameControl = new FormControl(this.data.lastname);
+  ageControl = new FormControl(this.data.age);
+
+  constructor(
+    public dialogRef: MatDialogRef<SeeUserDialog>,private userService: UsersService,
+    @Inject(MAT_DIALOG_DATA) public data: User,
+  ) {}
+
+  onCancel(): void {
+    this.dialogRef.close();
   }
 }
