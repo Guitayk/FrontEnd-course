d404275b58e06f8850f3d81a59d413274bf4f6af
vld <valentin.lorand@kereval.com>
2022-02-03 13:30:24 +0100

    Implémentation ajout d'une nouvelle association

--- fr-administration-front/src/app/associations-list/associations-list.component.ts
+++ fr-administration-front/src/app/associations-list/associations-list.component.ts
@@ -3,6 +3,12 @@ import { Association } from '../dto/Association';
 import { AssociationsService } from '../services/associations.service';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Observable, switchMap } from 'rxjs';
+import { AssociationForm } from '../dto/AssociationForm';
+import { VerbalProcess } from '../dto/VerbalProcess';
+import { User } from '../dto/User';
+import { UsersService } from '../services/users.service';
+import { MatListOption } from '@angular/material/list';
 
 @Component({
   selector: 'app-associations-list',
@@ -41,14 +47,24 @@ export class AssociationsListComponent implements OnInit {
 
   openAddDialog(): void {
     const dialogRef = this.dialog.open(AddAssociationDialog, {
+      height: "auto",
       width: '30em',
+
     });
     //TODO
     // dialogRef.afterClosed().subscribe(result => {});
   }
 
+  filterAssociations(name:string): Observable<Association[]> {
+    return this.associationService.searchAssociations({'name':name})
+  }
+
   ngOnInit(): void {
-    this.associationService.searchAssociations().subscribe(result => this.dataSource =result);
+    this.associationService.searchAssociations().subscribe(result => this.dataSource = result);
+
+    this.searchGroup.valueChanges.pipe(
+      switchMap(_=> this.filterAssociations(this.searchGroup.get("nameFilter")?.value))
+    ).subscribe(x => this.dataSource = x)
   }
 
 }
@@ -87,13 +103,40 @@ export class UpdateAssociationDialog {
 })
 export class AddAssociationDialog {
 
-  nameControl = new FormControl(this.data.name);
-  creationDateControl = new FormControl(this.data.dateOfCreation);
+  nameControl = new FormControl();
+  creationDateControl = new FormControl();
+  contentControl = new FormControl();
+  usersIDSelected : number[] |undefined;
+  assoForm : AssociationForm | undefined;
+  verbalProces : VerbalProcess | undefined;
+  users: User[] = [];
 
   constructor(
-    public dialogRef: MatDialogRef<AddAssociationDialog>,private associationService: AssociationsService,
+    public dialogRef: MatDialogRef<AddAssociationDialog>,private associationService: AssociationsService, private userService: UsersService,
     @Inject(MAT_DIALOG_DATA) public data: Association,
-  ) {}
+  ) {
+    this.userService.searchUsers().subscribe(x=> this.users=x);
+  }
+
+  validateDossier() {
+    this.associationService.createAssociationForm().subscribe(newAssoForm => this.assoForm = newAssoForm);
+  }
+  validateLegal() {   
+    this.associationService.validationLegalService(this.assoForm!).subscribe(newAssoForm => {this.assoForm = newAssoForm;console.log(this.assoForm)});
+    
+  }
+
+  validateFinancial() {    
+    this.associationService.validationFinancialService(this.assoForm!).subscribe(newAssoForm => this.assoForm = newAssoForm);
+  }
+
+  validateProcesVerbal() {
+    this.associationService.creationVerbalProcess(this.usersIDSelected!,this.contentControl.value,this.creationDateControl.value).subscribe(newVerbalProcess => this.verbalProces = newVerbalProcess);
+  }
+
+  onMembersListChange(options: MatListOption[]) {
+    this.usersIDSelected = options.map(o => o.value);
+  }
 
   cancel(): void {
     this.dialogRef.close();
