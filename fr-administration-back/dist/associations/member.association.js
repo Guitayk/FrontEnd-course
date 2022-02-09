032837ce823f703ea739ba9d16f874449d4a72ee
vld <valentin.lorand@kereval.com>
2022-02-09 14:02:54 +0100

    Add BACKEND

---
+++ fr-administration-back/dist/associations/member.association.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Member = void 0;
+class Member {
+    constructor(id, lastname, firstname, age, role) {
+        this.id = id;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.age = age;
+        this.role = role;
+    }
+}
+exports.Member = Member;
+//# sourceMappingURL=member.association.js.map
